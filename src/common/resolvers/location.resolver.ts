34e7d0d7cdd99f335613d37ca8233494b44afbb3
Yonathan Probeh <Probeh@outlook.com>
2021-04-10 18:42:07 +0300

    Application Update

--- src/common/resolvers/location.resolver.ts
+++ src/common/resolvers/location.resolver.ts
@@ -1,11 +1,11 @@
-import { Observable } from 'rxjs'
-import { mergeMap } from 'rxjs/operators'
-import { Injectable } from '@angular/core'
+import { Observable                              } from 'rxjs'
+import { mergeMap                                } from 'rxjs/operators'
+import { Injectable                              } from '@angular/core'
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router'
-import { GeoPosition } from '@models/geoPosition-model'
-import { Location } from '@models/location-model'
-import { ApplicationService } from '@services/application.service'
-import { WeatherService } from '@services/weather.service'
+import { GeoPosition                             } from '@models/geoPosition-model'
+import { Location                                } from '@models/location-model'
+import { ApplicationService                      } from '@services/application.service'
+import { WeatherService                          } from '@services/weather.service'
 
 @Injectable()
 export class LocationResolver implements Resolve<Location> {
@@ -13,18 +13,16 @@ export class LocationResolver implements Resolve<Location> {
   resolve(route: ActivatedRouteSnapshot): Observable<Location> {
     const params = route.queryParamMap;
 
-    /* Upon initial entry, request and process the user's location */
+    if (params.has('key'))
+      return this.weather.getLocation(params.get('key'));
+
     if (!this.application.geoPosition) {
       return this.application.getUserLocation()
         .pipe(mergeMap((position: GeoPosition) =>
           this.weather.getGeoPosition(position.latitude, position.longitude)));
     }
 
-    /* Upon navigation with provided location keys or coordinates as query params */
-    if (params.has('key'))
-      return this.weather.getLocation(params.get('key'));
-
-    else if (params.has('lat') && params.has('lng'))
+    if (params.has('lat') && params.has('lng'))
       return this.weather.getGeoPosition(+params.get('lat'), +params.get('lng'));
   }
 }
--- src/docs/js/menu-wc.js
+++ src/docs/js/menu-wc.js
@@ -249,9 +249,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
                             <li class="link">
-                                <a href="components/AppComponent.html" data-type="entity-link">AppComponent</a>
-                            </li>
-                            <li class="link">
                                 <a href="components/FavoritesComponent.html" data-type="entity-link">FavoritesComponent</a>
                             </li>
                             <li class="link">
